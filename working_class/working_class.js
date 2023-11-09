ee8caaab16af837d3e33239c494a2fddd3322e60
Mbarlowsg <130187725+Mbarlowsg@users.noreply.github.com>
2023-11-09 16:42:30 -0500

    "Play card" button now removes card from webpage
    New cards are automatically added to the cardDeck if its length drops below or is equal to the playerHandSize.
    
    Also fixed some bugs with the previous commit and cleaned up code.

--- working_class/working_class.js
+++ working_class/working_class.js
@@ -1,68 +1,45 @@
 /** @format */
 
 let cardDeck = [];
+let playedCards = [];
 let playerHandSize = 5;
+let playerHand = [];
 
 window.onload = () => {
-	fillCardDeck(10);
-	for (let i = 0; i < playerHandSize; i++) {
-		const currentCard = cardDeck.pop();
-		const cardElement = document.createElement("div");
-		cardElement.classList.add("card");
-		cardElement.classList.add("draggable");
-		cardElement.setAttribute("draggable", "true");
-		let elementHTML = `
-        <h1>${currentCard.value}</h1>
-    `;
-		cardElement.innerHTML = elementHTML;
-		setupModal2(cardElement, currentCard);
-
-		document.getElementById("card_container").appendChild(cardElement);
-	}
-	// setupModal();
+	fillCardDeck(6);
+	fillPlayerHand();
 };
 
-//
-function setupModal() {
-	const openButton = document.querySelectorAll(".card");
-	const modal = document.getElementById("card_modal");
-	const cardDisplay = document.getElementById("card_display");
-	const displayedHTML = `
-    <h1>openButton.value</h1>
-    <button data-close-modal>close</button>
-    `;
-
-	for (let i = 0; i < openButton.length; i++) {
-		openButton[i].addEventListener("click", () => {
-			modal.showModal();
-			modal.innerHTML = `
-		<h1>${cardDeck[i]}</h1>
-		<button data-close-modal>close</button>
-		`;
-			const closeButton = modal.querySelector("[data-close-modal]");
-			closeButton.onclick = () => {
-				modal.close();
-			};
-		});
+function checkCardDeck() {
+	if (cardDeck.length <= playerHandSize) {
+		fillCardDeck(5);
+		fillPlayerHand();
 	}
+	fillPlayerHand();
 }
 
-function setupModal2(cardElement, cardObject) {
-	const openButton = cardElement;
-	console.log(cardObject);
-	const modal = document.getElementById("card_modal");
+function fillPlayerHand() {
+	for (let i = 0; playerHand.length < playerHandSize; i++) {
+		const currentCard = cardDeck.shift();
+		const cardElement = document.createElement("div");
+		cardElement.classList.add("card");
+		cardElement.setAttribute("id", `card_${currentCard.id}`);
 
-	openButton.addEventListener("click", () => {
-		modal.showModal();
-		modal.innerHTML = `
-		<h1>${cardObject.value}</h1>
-		<button data-close-modal>close</button>
-		`;
-		const closeButton = modal.querySelector("[data-close-modal]");
-		closeButton.onclick = () => {
-			modal.close();
-		};
-	});
+		// used later for user to be able to rearrange deck
+		cardElement.classList.add("draggable");
+		cardElement.setAttribute("draggable", "true");
+
+		let elementHTML = `
+        <h1>${currentCard.value}</h1>
+        `;
+
+		cardElement.innerHTML = elementHTML;
+		setupModal(cardElement, currentCard);
+
+		document.getElementById("card_container").appendChild(cardElement);
+
+		playerHand.push(currentCard);
+	}
 }
 
 function fillCardDeck(numCards) {
@@ -70,7 +47,39 @@ function fillCardDeck(numCards) {
 		let cardObject = {
 			index: i,
 			value: Math.floor(Math.random() * 10),
+			id: Date.now() * i + 1,
 		};
 		cardDeck.push(cardObject);
 	}
+}
+
+function setupModal(cardElement, cardObject) {
+	const openButton = cardElement;
+	const modal = document.getElementById("card_modal");
+
+	openButton.addEventListener("click", () => {
+		modal.showModal();
+
+		// Displayed content
+		modal.innerHTML = `
+        <h1>${cardObject.value}</h1>
+        <button id="play_card_button" data-play-card>Play Card</button>
+        <button data-close-modal>close</button>
+        `;
+
+		const playCardButton = modal.querySelector("[data-play-card]");
+		playCardButton.onclick = () => {
+			modal.close();
+			playerHand.splice(playerHand.indexOf(cardObject), 1);
+			playedCards.push(cardObject);
+			document.getElementById(`card_${cardObject.id}`).remove();
+			checkCardDeck();
+		};
+
+		// Close Button logic (nested as it can only be set up once the modal is open)
+		const closeButton = modal.querySelector("[data-close-modal]");
+		closeButton.onclick = () => {
+			modal.close();
+		};
+	});
 }
