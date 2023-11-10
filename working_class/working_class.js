22de0159fb66d67e6ff238f0d24eae3d56afbd3c
Mbarlowsg <130187725+Mbarlowsg@users.noreply.github.com>
2023-11-09 19:02:43 -0500

    added default cards and reworked how cardDeck is filled
    created shuffleCards() which shuffles the cardDeck.

--- working_class/working_class.js
+++ working_class/working_class.js
@@ -1,19 +1,91 @@
 /** @format */
 
+// IK its named weirdly, it just helps it stick out.
+const working_class_cards = [
+	{
+		title: "Title 0",
+		index: 0,
+		id: 0,
+	},
+	{
+		title: "Title 1",
+		index: 1,
+		id: 1,
+	},
+	{
+		title: "Title 2",
+		index: 2,
+		id: 2,
+	},
+	{
+		title: "Title 3",
+		index: 3,
+		id: 3,
+	},
+	{
+		title: "Title 4",
+		index: 4,
+		id: 4,
+	},
+	{
+		title: "Title 5",
+		index: 5,
+		id: 5,
+	},
+	{
+		title: "Title 6",
+		index: 6,
+		id: 6,
+	},
+	{
+		title: "Title 7",
+		index: 7,
+		id: 7,
+	},
+	{
+		title: "Title 8",
+		index: 8,
+		id: 8,
+	},
+	{
+		title: "Title 9",
+		index: 9,
+		id: 9,
+	},
+];
+
 let cardDeck = [];
 let playedCards = [];
-let playerHandSize = 5;
 let playerHand = [];
 
+let playerHandSize = 5;
+
 window.onload = () => {
-	fillCardDeck(6);
+	fillCardDeck();
 	fillPlayerHand();
 };
 
+// fills cardDeck directly from working_class_cards
+function fillCardDeck() {
+	working_class_cards.forEach((card) => {
+		cardDeck.push(card);
+		shuffleCards();
+	});
+}
+
+// refills cardDeck with the already-played cards
+function refillCardDeck() {
+	playedCards.forEach((card) => {
+		cardDeck.push(card);
+		shuffleCards();
+	});
+	playedCards = [];
+}
+
+// Called every time a card is played
 function checkCardDeck() {
 	if (cardDeck.length <= playerHandSize) {
-		fillCardDeck(5);
-		fillPlayerHand();
+		refillCardDeck();
 	}
 	fillPlayerHand();
 }
@@ -30,7 +102,7 @@ function fillPlayerHand() {
 		cardElement.setAttribute("draggable", "true");
 
 		let elementHTML = `
-        <h1>${currentCard.value}</h1>
+        <h1>${currentCard.title}</h1>
         `;
 
 		cardElement.innerHTML = elementHTML;
@@ -42,14 +114,23 @@ function fillPlayerHand() {
 	}
 }
 
-function fillCardDeck(numCards) {
-	for (let i = 0; i < numCards; i++) {
-		let cardObject = {
-			index: i,
-			value: Math.floor(Math.random() * 10),
-			id: Date.now() * i + 1,
-		};
-		cardDeck.push(cardObject);
+// function testFillCardDeck(numCards) {
+// 	for (let i = 0; i < numCards; i++) {
+// 		let cardObject = {
+// 			index: i,
+// 			title: `test-${i}`,
+// 			id: Date.now() * i + 1,
+// 		};
+// 		cardDeck.push(cardObject);
+// 	}
+// }
+
+function shuffleCards() {
+	for (let i = cardDeck.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * i);
+		let temp = cardDeck[i];
+		cardDeck[i] = cardDeck[j];
+		cardDeck[j] = temp;
 	}
 }
 
@@ -62,17 +143,19 @@ function setupModal(cardElement, cardObject) {
 
 		// Displayed content
 		modal.innerHTML = `
-        <h1>${cardObject.value}</h1>
+        <h1>${cardObject.title}</h1>
         <button id="play_card_button" data-play-card>Play Card</button>
         <button data-close-modal>close</button>
         `;
 
+		// Play Card Button logic
 		const playCardButton = modal.querySelector("[data-play-card]");
 		playCardButton.onclick = () => {
 			modal.close();
 			playerHand.splice(playerHand.indexOf(cardObject), 1);
 			playedCards.push(cardObject);
 			document.getElementById(`card_${cardObject.id}`).remove();
+			console.log(cardDeck);
 			checkCardDeck();
 		};
 
